46451eea5c85c3d8b1a6b682bee3c2ff787c13a6
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-11-13 21:25:49 +0600

    Add files via upload

---
+++ abstrasion.p.py
@@ -0,0 +1,9 @@
+class student :
+    def __init__(self,name):
+        self.name = name 
+        print (self.name+ "online school")
+    def display ( self ):
+        print ("Running")
+r = student ("softmax")
+r.display()
+
