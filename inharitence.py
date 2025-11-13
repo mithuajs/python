8962b76481f4596a0debc86fa42d6d16c46ddda6
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-11-13 21:26:48 +0600

    Add files via upload

---
+++ inharitence.py
@@ -0,0 +1,11 @@
+class student :
+    name = ""
+    def result (self ) :
+        print ("3.80")
+class name (student):
+    def display (self):
+        print ("My name is"  , self.name )
+std = name ()
+std.name = "Mithu"
+std.result()
+std.display()
